715dde90a3e01a2e4d1be4c80c42fbfe87162e87
kissf <chois_lu@outlook.com>
2017-02-26 15:53:41 +0800

    testVCS

--- test.js
+++ test.js
@@ -2,7 +2,19 @@
  * Created by lujian on 2017-02-26.
  */
 
+var myObject = {
+    value: 100,
+    getValue:function () {
 
+        console.log(this.value); // 输出 100
 
+        // 输出 { value: 100, getValue: [Function] }，
+        // 其实就是 myObject 对象本身
+        console.log(this);
 
-console.log("ok")
+        return this.value;
+    }
+
+};
+
+console.log(myObject.getValue()); // => 100
