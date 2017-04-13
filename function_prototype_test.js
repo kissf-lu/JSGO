1b406f00868bd2f9991183a89c154cd8fb20d66c
kissf <chois_lu@outlook.com>
2017-04-13 19:00:56 +0800

    kissf emc6 class test

--- function_prototype_test.js
+++ function_prototype_test.js
@@ -2,13 +2,38 @@
  * Created by lujian on 2017-04-11.
  */
 
-function Obj(){
-    var a=0; //私有变量
-    var fn=function(){ //私有函数
-
-    }
+function Obj(a,b,c){
+    this.a=a; //私有变量
+    this.b=b;
+    this.c=c
 }
+Obj.prototype.fun_a=function () {
+    return this.b;
+};
+Obj.prototype.fun_b=function () {
+    return this.c;
+};
+var o=new Obj('a','b','c');
+console.log(o.a);
+console.log(o.fun_a());
+console.log(o.fun_b());
 
-var o=new Obj();
-console.log(o.a); //undefined
-console.log(o.fn); //undefined
+var p=new Obj('1','2','3');
+console.log(p.a);
+console.log(p.fun_a());
+console.log(p.fun_b());
+
+(function (a) {
+    console.log(a.a)
+})(p);
+function greet() {
+    var reply = [this.person, 'Is An Awesome', this.role].join(' ');
+    console.log(reply);
+}
+var i = {
+    person: 'Douglas Crockford', role: 'Javascript Developer'
+};
+
+greet.call(i); // Douglas Crockford Is An Awesome Javascript Developer
+
+console.log([].isPrototypeOf([]));
